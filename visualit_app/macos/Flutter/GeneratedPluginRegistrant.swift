ec085b1a053ae30a28d6a1d6bfaeade23a2a8a15
Vikkash2003 <gowryvikky2003@gmail.com>
2025-02-22 23:52:02 +0530

    firebase connectivity

--- visualit_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ visualit_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,8 @@ import Foundation
 
 import audioplayers_darwin
 import file_picker
+import firebase_auth
+import firebase_core
 import flutter_tts
 import path_provider_foundation
 import screen_brightness_macos
@@ -14,6 +16,8 @@ import screen_brightness_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
